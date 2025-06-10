9ce75ca0f71442810b8876c63de7422015e104b7
= <sarthak.rana0321@gmail.com>
2025-06-10 18:38:42 +0530

    added loader

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import React, { useState, type FormEvent } from "react"
+import { BarLoader } from "react-spinners";
 
 
 interface redirectObject {
@@ -12,9 +13,9 @@ interface redirectObject {
 const resutlArea = (data: redirectObject[]) => {
     return (
         <div className="w-full h-full flex flex-col items-center px-4 py-8">
-            <h2 className="text-2xl font-semibold mb-6 text-red-600">Faulty Redirect Results: {data.length}</h2>
+            <h2 className="text-2xl font-semibold mb-7 text-red-600">Faulty Redirect Results: {data.length}</h2>
 
-            <div className="w-full max-w-3xl h-90 space-y-6 overflow-y-scroll">
+            <div className="w-full max-w-3xl h-90 text-sm space-y-6 overflow-y-scroll transition delay-100 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110">
                 {data.map((item, index) => (
                 <div
                     key={index}
@@ -38,7 +39,8 @@ export default function Home() {
     const [file, setFile] = useState<File | null>(null)
     const [data, setData] = useState<redirectObject[]>([])
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const [status, setStatus] = useState<string>('');
+    const [loading, setLoading] = useState<boolean>(false);
+
 
     const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         if (e.target.files) {
@@ -53,7 +55,7 @@ export default function Home() {
             alert("Please upload an excel file")
             return
         }
-
+        setLoading(true)
         const formData = new FormData()
         formData.append('file', file, `${Date.now()}-${file.name}`)
 
@@ -65,11 +67,14 @@ export default function Home() {
                 }
             })
             // console.log('finised', res.data.data)
+            setLoading(false)
             setData(res.data.data)
 
         } catch (error) {
+            setLoading(false)
             alert("Something Went Wrong, Please try again")
             console.log(error)
+            
         }
         
     }
@@ -79,14 +84,14 @@ export default function Home() {
 
        <div className="mt-10">
          <form onSubmit={handleUpload} className="flex justify-center items-center flex-col gap-10">
-            <div className="w-1/3 border-2 h-32 border-dashed border-purple-500 rounded">
+            <div className="w-1/3 border-2 h-32 border-dashed border-purple-500 rounded transition delay-100 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110">
                 <input
-                    className="w-full h-full text-center cursor-pointer text-green-100" 
+                    className="w-full h-full text-lg content-center pl-[35%] cursor-pointer text-purple-400" 
                     type="file" 
                         accept=".xlsx,.xls"
                         onChange={handleFileChange}/>
             </div>
-            <button type="submit">Check Redirect</button>
+            <button type="submit">{loading ? <BarLoader loading height={6} color="#b58eff"/> : `Check Redirect`}</button>
         </form>
         {data.length > 0 && resutlArea(data)}
 
