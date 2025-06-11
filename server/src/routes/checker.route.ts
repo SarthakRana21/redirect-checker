d184d6e3178c1385112212d5db51eef14d662f33
= <sarthak.rana0321@gmail.com>
2025-06-11 11:27:12 +0530

    added check-redirect route

---
+++ server/src/routes/checker.route.ts
@@ -0,0 +1,13 @@
+import multer from "multer";
+import { redirectChecker } from "../controllers/checker.controller";
+import { Router } from "express";
+
+const checkerRouter = Router()
+const upload = multer({dest: 'upload/'})
+
+checkerRouter.route('/check-redirect').post(
+    upload.single('file'), 
+    redirectChecker
+)
+
+export default checkerRouter
