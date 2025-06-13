a684491af642d801e75fd4c423db00192183270e
SarthakRana21 <107024368+SarthakRana21@users.noreply.github.com>
2025-06-13 11:01:27 +0000

    udpated header

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -2,7 +2,7 @@ export default function Header() {
     return (
 
         <div>
-            <div className="w-full flex justify-center py-2.5">
+            <div className="w-full flex justify-center py-3 border-b-1 border-purple-600">
                 <h2 className="font-semibold text-3xl">Redirect Checker Web</h2>
             </div>
         </div>
