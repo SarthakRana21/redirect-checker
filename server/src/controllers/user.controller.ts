a5b1448959403c6a1986dcb4d9bf3589fbbd6792
SarthakRana21 <107024368+SarthakRana21@users.noreply.github.com>
2025-06-16 17:08:22 +0000

    registeruser function

--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -4,6 +4,7 @@ import { users } from "../models/users.model"
 import { ApiError } from "../utils/ApiError"
 import { generateAccessToken, generateRefreshToken } from "../utils/tokens/TokenGenerator"
 import { asyncHandler } from "../utils/AysncHandler"
+import bcrypt from 'bcrypt'
 
 const generateRefreshAndAccessTokens = async(userId: number) => {
     try {
@@ -27,11 +28,48 @@ const generateRefreshAndAccessTokens = async(userId: number) => {
 
 const registerUser = asyncHandler(async (req, res) => {
     const {username, email, password} = req.body
-    const useFileds = [username, email, password]
+    const userFields = [username, email, password]
 
     try {
+        if (userFields.some((item) => !item || item?.trim() === "")) {
+            throw new ApiError(400, "All fields are required")
+        }
         
+        const user = await db.select().from(users).where(eq(users.email, email))
+
+        if(!user) {
+            const hashedPass = await bcrypt.hash(password, 10)
+
+            const newRecord = await db.insert(users).values({
+                username,
+                email,
+                password: hashedPass
+            })
+            const {accessToken, refreshToken} = await generateRefreshAndAccessTokens(newRecord.lastInsertRowid)
+
+            const updatedRecord = await db.update(users).set({refreshToken}).where(eq(users.id, newRecord.lastInsertRowid))
+
+            const options = {
+                httpOnly: true,
+                secure: true
+            }
+            return res.status(200)
+            .cookie("accessToken", accesstoken, options)
+            .cookie("refreshToken", refreshToken, options)
+            .json(
+                new ApiResponse(200, {
+                    username,
+                    email,
+                }, "User Register Successfully")
+            )
+
+        }
+        return res.status(409).json(
+            new ApiResponse(409, "User already Exist")
+        )
+
     } catch (error) {
         console.log( "Error while regestring user",error)
+        throw new ApiError(500, "something went wrong while registering a user")
     }
 })
