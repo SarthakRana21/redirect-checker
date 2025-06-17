00632a97dff257da7ae26cf58892c06c54502e61
= <sarthak.rana0321@gmail.com>
2025-06-17 18:06:09 +0530

    updated field fullName to not unique

--- server/src/models/users.model.ts
+++ server/src/models/users.model.ts
@@ -2,7 +2,7 @@ import { sqliteTable, text, integer } from "drizzle-orm/sqlite-core";
 
 export const users = sqliteTable('users', {
     id: integer('id').primaryKey({autoIncrement: true}),
-    fullName: text('full_name').notNull().unique(),
+    fullName: text('full_name').notNull(),
     email: text('email').notNull().unique(),
     password: text('password').notNull(),
     refreshToken: text('refresh_token'),
