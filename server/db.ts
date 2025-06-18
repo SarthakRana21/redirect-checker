013e59f7498a88edb2b856001f60caa3c2b1d2f7
= <sarthak.rana0321@gmail.com>
2025-06-18 11:54:19 +0530

    updated db.ts

--- server/db.ts
+++ server/db.ts
@@ -1,9 +1,9 @@
 import { drizzle } from "drizzle-orm/better-sqlite3";
 import Database from "better-sqlite3";
-import { users } from "../models/users.model";
-import { records } from "../models/records.model";
+import { users } from "./src/models/users.model";
+import { records } from "./src/models/records.model";
 
-const sqlite = new Database("redirect-checker.db")
+const sqlite = new Database("./drizzle/redirect-checker.db")
 export const db = drizzle(sqlite, {schema: {
     users, 
     records
