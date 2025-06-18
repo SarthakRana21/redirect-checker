0497f5a7f19eab8ceb1e7a07b04b973ea0763c47
= <sarthak.rana0321@gmail.com>
2025-06-18 11:55:33 +0530

    added user login route

--- server/src/routes/user.routes.ts
+++ server/src/routes/user.routes.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller";
+import { loginUser, registerUser } from "../controllers/user.controller";
 
 const userRouter = Router();
 
 // http://localhost:5000/api/v1/user/register
 userRouter.route('/register').post(registerUser)
+
+// http://localhost:5000/api/v1/user/login
+userRouter.route('/login').post(loginUser)
 
 
 export default userRouter
