b0f3d05ff3855c97af9463818d92f3beb0ff944c
= <sarthak.rana0321@gmail.com>
2025-06-19 18:41:29 +0530

    added login, regester, my account buttons

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,10 +1,30 @@
-export default function Header() {
-    return (
+import { useEffect, useState } from "react";
+import { useProfile } from "./hooks/useProfile.hook"
+import { LoginButton, MyAccountButton, RegisterButton } from "./buttons.list";
 
-        <div>
-            <div className="w-full flex justify-center py-3 border-b-1 border-purple-600">
-                <h2 className="font-semibold text-3xl">Redirect Checker Web</h2>
+
+export default function Header() {
+    const [loggedin, setLoggedin] = useState(false)
+    const { data: profile } = useProfile();
+
+    useEffect(() => {
+        setLoggedin(!!profile);
+    }, [profile]);
+    console.log(profile)
+
+    return (
+        <div className="w-full py-4 flex justify-center items-center border-b border-purple-600">
+            <div className="w-[80%] flex justify-between items-center">
+                <h2 className="font-semibold text-2xl">Redirect Checker Web</h2>
+                <div className="flex items-center justify-between gap-4">
+                    {loggedin ? <MyAccountButton /> : (
+                        <>
+                            <LoginButton />
+                            <RegisterButton />
+                        </>
+                    )}
+                </div>
             </div>
         </div>
-    )
+    );
 }
---
+++ client/src/components/buttons.list.tsx
@@ -0,0 +1,59 @@
+const LoginButton: React.FC = () => {
+  return (
+    <div className="flex justify-center">
+      <button
+        className="
+          px-6 py-2 rounded-xl text-sm font-medium text-white
+          bg-gradient-to-r from-slate-700 to-slate-600
+          shadow-md
+          transition-all duration-300
+          hover:bg-gradient-to-r hover:from-slate-600
+          "
+      >
+        Login
+      </button>
+    </div>
+  );
+};
+
+const RegisterButton: React.FC = () => {
+  return (
+    <div className="flex justify-center">
+      <button
+        className="
+          px-6 py-2 rounded-xl text-sm font-medium text-white
+          bg-gradient-to-r from-indigo-600 to-purple-600
+          shadow-md
+          transition-all duration-300
+          hover:bg-gradient-to-r hover:from-indigo-500
+          "
+      >
+        Register
+      </button>
+    </div>
+  );
+};
+
+const MyAccountButton: React.FC = () => {
+  return (
+    <div className="flex justify-center">
+      <button
+        className="
+          px-6 py-2 rounded-xl text-sm font-medium text-white
+          bg-gradient-to-r from-slate-700 to-slate-600
+          shadow-md
+          transition-all duration-300
+          hover:bg-gradient-to-r hover:from-slate-600
+          "
+        >
+        My Account
+      </button>
+    </div>
+  );
+};
+
+export {
+    LoginButton,
+    RegisterButton,
+    MyAccountButton
+}
