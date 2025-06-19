6eca709f3cb91647cbf59bfb487aa5f887031811
= <sarthak.rana0321@gmail.com>
2025-06-19 19:19:26 +0530

    added react router dom

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Header from "./components/Header"
 import Home from "./components/Home"
+import LoginPage from "./components/pages/Login"
 
 function App() {
 
   return (
     <>
-     <Header />
-     <Home />
+     <BrowserRouter>
+      <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
+        
+     </BrowserRouter>
     </>
   )
 }
