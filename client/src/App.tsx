6541776b1e083637b00f44272fba0c7f7745c9d0
= <sarthak.rana0321@gmail.com>
2025-06-20 14:52:37 +0530

    added register and profile

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Header from "./components/Header"
 import Home from "./components/Home"
-import LoginPage from "./components/pages/Login"
+import LoginPage from "./components/pages/Login.page"
+import RegisterPage from "./components/pages/Register.page"
+import ProfilePage from "./components/pages/Profile.page"
 
 function App() {
 
@@ -12,6 +14,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
         </Routes>
         
      </BrowserRouter>
