e7d0e5ba62a516de0770486e52cf9f109610cdde
= <sarthak.rana0321@gmail.com>
2025-06-20 14:53:28 +0530

    udpated useProfile hook

--- client/src/components/hooks/useProfile.hook.ts
+++ client/src/components/hooks/useProfile.hook.ts
@@ -1,18 +1,24 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
-const fetchProfile = async () => {
-        
-    const { data } = await axios.get(`${import.meta.env.VITE_API_URL}user/profile`)
-
-    return data
+export const fetchProfile = async () => {
+    try {
+        const { data } = await axios.get(`${import.meta.env.VITE_API_URL}user/profile`,{
+        withCredentials: true
+        })
+        // console.log("fetchprofile",data)
+        return data
+    } catch(error) {
+        return error
+    }
 }
 
-export const useProfile = () => {
+export const useProfile = (options = {}) => {
     return useQuery({
         queryKey: ['profile'],
         queryFn: fetchProfile,
         staleTime: 1000 * 60 * 60 * 6, // 6 hours
         retry: false,
+        ...options
     })
 }
