9a123fb2f4cd7b4b63a2adc0b07ed9460f2fdc7f
= <sarthak.rana0321@gmail.com>
2025-06-23 13:27:47 +0530

    added new fields

--- server/src/models/records.model.ts
+++ server/src/models/records.model.ts
@@ -4,5 +4,8 @@ import { users } from "./users.model";
 export const records = sqliteTable('records', {
     id: integer('id').primaryKey({autoIncrement: true}),
     userId: integer('user_id').notNull().references(() => users.id),
-    data: text('data')
+    jobId: text('job_id').notNull().unique(),
+    data: text('data'),
+    status: text('status'),
+    createdAt: text('created_at').notNull()
 }) 
