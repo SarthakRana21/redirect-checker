18edd0cc7d2b8db7a90d00e0b1ad4afebdd88ab9
SarthakRana21 <107024368+SarthakRana21@users.noreply.github.com>
2025-06-23 05:32:26 +0000

    updated checker controller to worker

--- server/src/controllers/checker.controller.ts
+++ server/src/controllers/checker.controller.ts
@@ -1,89 +1,27 @@
-import xlsx from 'xlsx';
-import axios from 'axios';
-import fs from 'fs/promises';
 import { asyncHandler } from '../utils/AysncHandler';
 import { ApiError } from '../utils/ApiError';
 import { ApiResponse } from '../utils/ApiResponse';
+import { redirectQueue } from '../jobs/queue';
+import { AuthRequest } from '../interfaces';
+import { JwtPayload } from 'jsonwebtoken';
 
-interface redirectObject {
-    address: string;
-    status_code?: number;
-    redirect_url: string;
-    expected_url?: string;
-}
 
-function wait(ms: number) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-export const redirectChecker = asyncHandler(async (req, res) => {
+export const redirectChecker = asyncHandler(async (req: AuthRequest, res) => {
     const path = req.file?.path
+    const user = req.user as JwtPayload
+
     if (!path) throw new ApiError(400, "No file uploaded")
 
-    const result: redirectObject[] = [];
-    const workbook = xlsx.readFile(path)
-    const sheet = workbook.Sheets[workbook.SheetNames[0]];
-    const data = xlsx.utils.sheet_to_json(sheet);
-
     try {
-        for (let i = 0; i < data.length; i++) {
-        const item = data[i] as redirectObject
-        const response = await axios.get(item.address, {
-            maxRedirects: 0,
-            validateStatus: () => true
-        })
-        if (response.headers.location !== item.redirect_url) {
-            result.push({
-                address: item.address,
-                status_code: response.status || 0,
-                redirect_url: response.headers.location,
-                expected_url: item.redirect_url
-            })
-        }
-        await wait(Math.floor(Math.random() * (7000 - 3000 + 1)) + 3000)
-    }
+        const job = await redirectQueue.add('redirect-check', {filePath: path})
+
+
+
     } catch (error) {
-        console.log(error)
         throw new ApiError(402, error as string)
     }
-    await fs.unlink(path)
+   // await fs.unlink(path)
     // console.log(result)
-    return res.status(200).json(new ApiResponse(200, result))
+   // return res.status(200).json(new ApiResponse(200, result))
 })
-
-
-
-// export const redirectChecker = async (path: string) => {
-//     console.log('working')
-//     const result: redirectObject[] = [];
-//     const workbook = xlsx.readFile(path)
-//     const sheet = workbook.Sheets[workbook.SheetNames[0]];
-//     const data = xlsx.utils.sheet_to_json(sheet);
-    
-//     try {
-//         for (let i = 0; i < data.length; i++) {
-//         const item = data[i] as redirectObject
-//         const response = await axios.get(item.address, {
-//             maxRedirects: 0,
-//             validateStatus: () => true
-//         })
-//         if (response.headers.location !== item.redirect_url) {
-//             result.push({
-//                 address: item.address,
-//                 status_code: response.status || 0,
-//                 redirect_url: response.headers.location,
-//                 expected_url: item.redirect_url
-//             })
-//         }
-//         await wait(3000)
-//     }
-//     } catch (error) {
-//         console.log(error)
-//         return error
-//     }
-//     await fs.unlink(path)
-//     // console.log(result)
-//     return result
-
-// }
 
