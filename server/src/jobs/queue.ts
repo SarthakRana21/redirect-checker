72303887785bb6070a590b7fec27d47450806def
SarthakRana21 <107024368+SarthakRana21@users.noreply.github.com>
2025-06-23 05:31:57 +0000

    added queue and worker

---
+++ server/src/jobs/queue.ts
@@ -0,0 +1,10 @@
+import { Queue } from 'bullmq';
+import Redis from 'ioredis';
+
+export const connection = {
+  host: '127.0.0.1',
+  port: 6379,
+};
+
+
+export const redirectQueue = new Queue('redirect-check', { connection });
