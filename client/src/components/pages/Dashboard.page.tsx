cea7b8870fd8ae5308895ea4ce4caf0b7615fe66
= <sarthak.rana0321@gmail.com>
2025-06-24 19:13:16 +0530

    added table and job fetch

--- client/src/components/pages/Dashboard.page.tsx
+++ client/src/components/pages/Dashboard.page.tsx
@@ -1,24 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import { useProfile } from "../hooks/useProfile.hook";
+import SimpleTable from "../table";
+import axios from "axios";
+import type { tableData } from "../interfaces/interface";
+
 
 const Dashboard: React.FC = () => {
+  
+  const [data, setData ] = useState<tableData[]>([])
   const { data: profile} = useProfile();
     
-  console.log([profile])
+  // console.log([profile])
   const name = profile?.data[0]?.fullName
   const email = profile?.data[0]?.email
 
-//   console.log(profile?.data[0])
+  // console.log(profile?.data[0])
+
+  axios.get(`${import.meta.env.VITE_API_URL}jobs`, {
+      withCredentials: true
+  })
+  .then(response => {
+      setData(response.data.data)
+  })
 
   return (
-    <div className="min-h-[70vh] flex justify-center items-center bg-gray-900 px-4">
-      <div className="bg-gray-800 w-full max-w-md rounded-lg shadow-lg p-8 text-white">
+    <div className="min-h-full w-full flex mt-10 flex-col gap-10 justify-center items-center bg-gray-900 px-4">
+      <div className="bg-gray-800 w-1/2 rounded-lg shadow-lg py-10 px-20 text-white">
         <h2 className="text-3xl font-semibold mb-6 text-center">My Profile</h2>
 
         <div className="space-y-4">
           <ProfileItem label="Full Name" value={name || "N/A"} />
           <ProfileItem label="Email" value={email || "N/A"} />
           {/* Add more fields as needed */}
+        </div>
+      </div>
+
+      <div className="w-full flex justify-center items-center">
+        <div className="bg-gray-800 w-1/2 rounded-lg shadow-lg p-8 text-white">
+          <h2 className="text-3xl font-semibold mb-6 text-center flex justify-center items-center gap-3">History
+          </h2>
+
+          {data.length > 0 ? <SimpleTable data={data} /> : <div>No Records Found</div>}
+
         </div>
       </div>
     </div>
